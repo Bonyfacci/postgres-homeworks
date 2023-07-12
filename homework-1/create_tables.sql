e7e402f70e55c944dfa85a9ac3d5445079a873b9
Сергей Платонов <platonovpochta@gmail.com>
2023-07-12 18:45:00 +0200

    Homework 1 CREATE TABLES add info in tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+	employee_id int NOT NULL PRIMARY KEY,
+	first_name varchar(25) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date DATE,
+	notes text
+);
+
+CREATE TABLE orders (
+	order_id int PRIMARY KEY,
+	customer_id varchar(5),
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date DATE,
+	ship_city varchar(50),
+    UNIQUE(customer_id)
+);
+
+CREATE TABLE customers (
+    customer_id varchar(5) REFERENCES orders(customer_id) NOT NULL,
+    company_name varchar(50),
+    contact_name varchar(50)
+)
